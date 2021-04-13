d89e97ae9ebdc647e15463780b726b54fa76e9e1
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-04-14 00:21:20 +0300

    add complate task-8

--- js/task-08.js
+++ js/task-08.js
@@ -3,28 +3,35 @@ const renderBtn = document.querySelector('[data-action="render"]');
 const destroyBtn = document.querySelector('[data-action="destroy"]');
 const createDivBox = document.querySelector('#boxes');
 
-renderBtn.addEventListener('click', createBoxes);
+function createBoxes(amount) {
+    const array = [];
+    let counterWidth = 20;
+    let counterHeigth = 20;
+    const randomRgb = () => {
+        const rgbNum = () => Math.floor(Math.random() * 256);
+        const r = rgbNum();
+        const g = rgbNum();
+        const b = rgbNum();
+        return `rgb(${r},${g},${b})`;
+    };
+    const getRandomColor = randomRgb;
+    for (let i=0; i<amount; i+=1) {
+        array.push(`<div id="newDiv" style="width: ${(counterWidth += 10)}px;
+        height: ${(counterHeigth += 10)}px; background: ${getRandomColor()}"></div>`)
+    }
+    return array.reduce((acc, el) => acc += el, '');
+};
 
-function createBoxes() {
-    inputEl.addEventListener('input', (amount) => {
-        const array = [];
-        array.length = amount.currentTarget.value;
-    })
-   
-    console.log(amount.currentTarget.value);
-    // array.length = amount.currentTarget.value;
-    // console.log(array);
+const createBoxesWithArray = createBoxes();
+
+renderBtn.addEventListener('click', () => {
+    createDivBox.insertAdjacentHTML('beforeend', (createBoxes(inputEl.value)))
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+   let deleteDiv = document.querySelectorAll('#newDiv');
+   deleteDiv.forEach(el => el.remove());
+   inputEl.value = '';
 }
-
-
-// function destroyBoxes() {
-
-// }
-
-// const randomRgb = () => {
-//     const rgbNum = () => Math.floor(Math.random() * 256);
-//     const r = rgbNum();
-//     const g = rgbNum();
-//     const b = rgbNum();
-//     return `rgb(${r},${g},${b})`;
-// };
